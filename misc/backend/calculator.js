5b18e7fb9238d9cc1540e90c8dc65191e9d8a42a
CitizenThreee <albeisly@gmail.com>
2024-05-01 13:26:33 +1200

    calculator

--- misc/backend/calculator.js
+++ misc/backend/calculator.js
@@ -8,4 +8,45 @@ function Sum(args){
     return sum;
 }
 
-console.log(Sum([3, 1, 5, 7, 8]));
+function Sub(args){
+    let sub = 0;
+
+    for(let i = 0; i < args.length; i++){
+        sub -= args[i];
+    }
+
+    return sub;
+}
+
+function Div(args){
+    let div = args[0];
+
+    for(let i = 1; i < args.length; i++){
+        div /= args[i];
+    }
+
+    return div;
+}
+
+function Mul(args){
+    let mul = args[0];
+
+    for(let i = 1; i < args.length; i++){
+        mul *= args[i];
+    }
+
+    return mul;
+}
+
+function Dif(a, b){
+    let dif = 0; 
+
+    for(let i = 0; i < args.length; i++){
+        sum -= args[i];
+    }
+
+    return dif;
+}
+
+console.log(Mul([4, 6, 1, 7]));
+
